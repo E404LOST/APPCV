d375e2bc451da83af12955ca7234ac4c1c0aa034
E404LOST <115174841+E404LOST@users.noreply.github.com>
2022-11-21 12:51:39 -0500

    Add files via upload

---
+++ noad.js
@@ -0,0 +1,8 @@
+/* Template: Sync - Free Mobile App Landing Page HTML Template
+   Author: Inovatik
+   Created: Dec 2019
+   Description: Custom JS file
+*/
+
+
+console.log(delete e.src);   // retorna true
